106c1f07e78fd96e141c3ecd84b3b74ae9e1b8bb
lemonade <32491056+lem-onade@users.noreply.github.com>
2020-02-26 15:02:51 +0100

    Update OrganisationGetr.py

--- OrganisationGetr.py
+++ OrganisationGetr.py
@@ -4,13 +4,10 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 # GitHub login (fallback) LOGIN CREDENTIALS HERE #######################################################################
-# username = input("GitHub username:")
-# password = input("GitHub password:") # DEPRECATED!
 username = ''
 headers = {'Authorization': 'token '}
 
 # request all organisations ############################################################################################
-# response_orgs = requests.get('https://api.github.com/user/orgs', auth=HTTPBasicAuth(username, password))
 response_orgs = requests.get('https://api.github.com/user/orgs', headers=headers)
 orgs = response_orgs.json()
 
